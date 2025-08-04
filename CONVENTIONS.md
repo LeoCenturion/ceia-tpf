414c87aa80196c575cf1617c725b4900f897ed35
leo centurion <centurionm.leo@gmail.com>
2025-08-04 03:11:33 -0300

    Commit

---
+++ CONVENTIONS.md
@@ -0,0 +1,1 @@
+- Use type hints whenever possible
--- models/utils.py
+++ models/utils.py
@@ -1,3 +1,6 @@
+from types import LambdaType
+from typing import Callable, Optional, Sequence
+from matplotlib.axes import Axes
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -5,6 +8,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import kurtosis, skew
 from collections import namedtuple
 import os
+import itertools as it
 
 Variable = namedtuple("Variable", ["column", "label", "type"])
 
@@ -13,25 +17,85 @@ def prepare_df(df):
     df['diff'] =  df['close'] - df['open']
     df['dow'] = df['open_date'].dt.weekday
 
-def read_values() -> dict[int, dict[int, pd.DataFrame]]:
-    data_path = '../data/binance/python/data/spot/monthly/klines/BTCUSDT/1h/'
-    data_dict = {}
+def read_data(data_path: str ) -> pd.DataFrame:
+    # data_path = '../data/binance/python/data/spot/monthly/klines/BTCUSDT/1h/'
 
-    for filename in os.listdir(data_path):
-        if filename.endswith('.csv'):
-            parts = filename.split('-')
-            year = int(parts[2])
-            month = int(parts[3].split('.')[0])
+    if os.path.isdir(data_path):
+        # data_dict = {}
+        final_df = pd.DataFrame()
+        for filename in os.listdir(data_path):
+            if filename.endswith('.csv'):
+                # parts = filename.split('-')
+                # year = int(parts[2])
+                # month = int(parts[3].split('.')[0])
+                file_path = os.path.join(data_path, filename)
+                final_df = pd.concat([final_df, pd.read_csv(file_path)])
+                # df.columns = ["open_time", "open", "high", "low", "close", "volume", "close_time","quote_assets_volume", "number_of_trades", "taker_buy_base_asset_vol", "taker_buy_quote_asset_vol", "ignore"]
+                # prepare_df(df)
+                # if year not in data_dict:
+                #     data_dict[year] = {}
+                #     data_dict[year][month] = df
+        return final_df
+    else:
+        return pd.read_csv(data_path)
 
-            file_path = os.path.join(data_path, filename)
-            df = pd.read_csv(file_path)
-            df.columns = ["open_time", "open", "high", "low", "close", "volume", "close_time","quote_assets_volume", "number_of_trades", "taker_buy_base_asset_vol", "taker_buy_quote_asset_vol", "ignore"]
-            prepare_df(df)
-            if year not in data_dict:
-                data_dict[year] = {}
-            data_dict[year][month] = df
+def preprocess_data(
+        data: pd.DataFrame,
+        time_column: Optional[str] = None,
+        inplace: bool=False
+) -> pd.DataFrame:
 
-    return data_dict
+    df = data if inplace else data.copy()
+    if time_column != None:
+        df[time_column] = pd.to_datetime(df[time_column])
+        df.set_index(time_column, inplace=True)
+    return df
+
+def describe_instrument(
+        df: pd.DataFrame,
+        figsize=(11,5),
+        ncols=2,
+        time_variable = "date"
+):
+    def plot_variable_history(df: pd.DataFrame, ax: Axes, variable: str):
+        sns.lineplot(df, x = time_variable, y = variable, ax=ax)
+        ax.set_title(f"{variable} history")
+
+    plot_price_history = lambda ax: plot_variable_history(df, ax, "close")
+    plot_returns_history = lambda ax: plot_variable_history(df, ax, "returns")
+    plot_volatility_history = lambda ax: plot_variable_history(df, ax, "volatility")
+    plot_return_std_history = lambda ax: plot_variable_history(df, ax, "return_std")
+
+    plots = [
+        plot_price_history, plot_returns_history ,
+        plot_volatility_history, plot_return_std_history
+    ]
+    plots = list(it.batched(plots, n=ncols))
+
+    return plot_grid(
+        plotters=plots,
+        figsize=figsize,
+    )
+
+
+def plot_grid(
+        plotters: Sequence[Sequence[Callable[[Axes], None]]],
+        figsize=(11,5),
+):
+    ncols=len(plotters[0])
+    nrows=len(plotters)
+    fig, axs = plt.subplots(
+        ncols=ncols,
+        nrows=nrows,
+        figsize=figsize,
+        layout="constrained"
+    )
+
+    for row in range(nrows):
+        for col in range(ncols):
+            ax = axs[row, col]
+            plotters[row][col](ax)
+    return fig, axs
 
 def compute_runs(df):
     start_prices = []
@@ -94,3 +158,4 @@ def compute_runs(df):
          'end_time': end_times})
     result['run_diff'] = result['end_price'] - result['start_price']
     return result
+
